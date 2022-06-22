99786cce931ba5e6aa65ced8ab55c91437a53382
Wilson Bertin <bertinfilho@gmail.com>
2022-06-22 16:48:42 -0300

    2022_06_22 009 -> Abstracao de Funcionario.kt

--- src/main/kotlin/one/digitalinnovations/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovations/digionebank/Funcionario.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovations.digionebank
 import java.math.BigDecimal
 
-class Funcionario(
-    override val nome : String,
-    override val cpf : String,
+abstract class Funcionario(
+    nome : String,
+    cpf : String,
     val salario : BigDecimal
 
 ) : Pessoa(nome, cpf){
