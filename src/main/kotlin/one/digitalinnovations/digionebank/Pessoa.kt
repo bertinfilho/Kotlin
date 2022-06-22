23fbb7f3aa8fc6946fcb83d1bbff38652e2aff39
Wilson Bertin <bertinfilho@gmail.com>
2022-06-22 16:48:13 -0300

    2022_06_22 008 -> Abstracao de Pessoa.kt

--- src/main/kotlin/one/digitalinnovations/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovations/digionebank/Pessoa.kt
@@ -1,10 +1,8 @@
 package one.digitalinnovations.digionebank
 
-open class Pessoa (
-    open val nome: String,
-    open val cpf: String
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
     //private set **  // private set ** -> Aplicado abaixo da var bloqueia so a var acima
-
-
 )
 
