6756657ad2ddc26649a122fb41d4b3794657d444
Wilson Bertin <bertinfilho@gmail.com>
2022-06-22 09:58:12 -0300

    2022_06_22 003 -> Cria classe conta e estrutura basica

---
+++ src/main/kotlin/one/digitalinnovations/digeonebank/conta.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovations.digeonebank
+import java.math.BigDecimal
+
+class conta {
+    val agencia: String,
+    val numero:String,
+    val saldo:BigDecimal
+
+
+} {
+    fun deposito(valor:BigDecimal){
+
+    }
+
+    fun saque(valor:BigDecimal){
+
+    }
+
+}
